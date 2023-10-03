9eb295f2de8390035a07b23fde6fa7ae14981d7b
cam <cameron.ryan.pears@gmail.com>
2023-10-03 16:26:44 +1300

    Check that dnsutils is installed

--- _release/postinstall.sh
+++ _release/postinstall.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+
+# Check that dnsutils is installed
+if ! command -v nslookup &> /dev/null; then
+  echo "nslookup not found, can be installed with: sudo apt install dnsutils"
+  exit 1
+fi
+
 systemctl daemon-reload
 systemctl enable modemd.service
 systemctl restart modemd.service
